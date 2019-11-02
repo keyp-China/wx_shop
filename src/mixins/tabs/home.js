f7e3cb05a1eb05f7b5246031a3f4c5471800da49
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-11-02 16:25:49 +0800

    js数据提取
    js数据提取

---
+++ src/mixins/tabs/home.js
@@ -0,0 +1,45 @@
+import wepy from 'wepy'
+
+export default class extends wepy.mixin {
+    data = {
+        swiperData: [], //轮播图数据
+        catesData: [] //分类数据
+    }
+
+    config = {}
+
+    methods = {}
+
+    /**
+     *  获取轮播图数据 
+     */
+    async getSwiperData() {
+        const { data } = await wepy.request({
+            url: "https://uinav.com/api/public/v1/home/swiperdata"
+        })
+        if (data.meta.status !== 200) {
+            return console.log("获取轮播图失败");
+        }
+        this.swiperData = data.message
+        this.$apply()
+    }
+
+    /**
+     *  获取分类数据
+     */
+    async getCateData() {
+        const { data } = await wepy.request({
+            url: "https://uinav.com/api/public/v1/home/catitems"
+        })
+        if (data.meta.status !== 200) {
+            return console.log("获取分类失败");
+        }
+        this.catesData = data.message
+        this.$apply()
+    }
+
+    onLoad() {
+        this.getSwiperData()
+        this.getCateData()
+    }
+}
