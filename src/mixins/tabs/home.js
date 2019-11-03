3ffcfd68b4b1a5fa998c77cf3f3bb5314dbfff86
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-11-03 23:51:31 +0800

    首页-楼层
    首页-楼层

--- src/mixins/tabs/home.js
+++ src/mixins/tabs/home.js
@@ -3,7 +3,8 @@ import wepy from 'wepy'
 export default class extends wepy.mixin {
     data = {
         swiperData: [], //轮播图数据
-        catesData: [] //分类数据
+        catesData: [], //分类数据
+        floorData: [] //楼层数据
     }
 
     config = {}
@@ -34,8 +35,21 @@ export default class extends wepy.mixin {
         this.$apply()
     }
 
+    /**
+     *  获取楼层数据
+     */
+    async getFloorData() {
+        const { data } = await wepy.get("/home/floordata")
+        if (data.meta.status !== 200) {
+            return wepy.baseToast("分类楼层数据失败")
+        }
+        this.floorData = data.message
+        this.$apply()
+    }
+
     onLoad() {
         this.getSwiperData()
         this.getCateData()
+        this.getFloorData()
     }
 }
