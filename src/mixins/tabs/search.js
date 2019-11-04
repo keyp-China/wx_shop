6e739e4bca8ec1ba9fbbee68a51bd4ec10c8b047
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-11-04 20:02:58 +0800

    搜索-历史记录完成
    搜索-历史记录完成

--- src/mixins/tabs/search.js
+++ src/mixins/tabs/search.js
@@ -3,7 +3,8 @@ import wepy from 'wepy'
 export default class extends wepy.mixin {
     data = {
         value: '',
-        searchData: []
+        searchData: [],
+        history: wepy.getStorageSync('history') || []
     }
 
     config = {
@@ -20,9 +21,27 @@ export default class extends wepy.mixin {
         // 点击搜索按钮
         onSearch() {
             if (this.value.trim().length == 0) return
+            this.history.unshift(this.value)
+            wepy.setStorageSync('history',this.history.slice(0,10))
             wepy.navigateTo({
                 url: `/pages/goods_list/index?query=${this.value}`
             })
+        },
+
+        //点击搜索记录
+        onTapTag(index){
+            const delItem = this.history.splice(index,1)
+            this.history.unshift(delItem[0]);
+            wepy.setStorageSync('history',this.history)
+            wepy.navigateTo({
+                url: `/pages/goods_list/index?query=${delItem[0]}`
+            })
+        },
+
+        //删除历史记录
+        delHistory(){
+            this.history = []
+            wepy.removeStorageSync('history');
         }
     }
 
@@ -35,4 +54,5 @@ export default class extends wepy.mixin {
         this.searchData = data.message
         this.$apply()
     }
+
 }
