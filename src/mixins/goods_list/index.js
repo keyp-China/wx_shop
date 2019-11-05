a31e11e9e6f22902b12b4492f74dcfff0041e156
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-11-05 14:56:04 +0800

    商品列表-下拉刷新&页面跳转商品详情
    商品列表-下拉刷新&页面跳转商品详情

--- src/mixins/goods_list/index.js
+++ src/mixins/goods_list/index.js
@@ -13,13 +13,20 @@ export default class extends wepy.mixin {
     }
 
     methods = {
-
+        /**
+         * 跳转到商品详情页
+         */
+        goGoodsDetail(goods_id){
+            wepy.navigateTo({
+                url:`/pages/goods_detail/index?goods_id=${goods_id}`
+            })
+        }
     }
 
     /**
      * 获取商品列表数据
      */
-    async getGoodsList(){
+    async getGoodsList(callback){
         this.isLoading = true;
         const { data } = await wepy.get('/goods/search',{
             query: this.query,
@@ -27,6 +34,8 @@ export default class extends wepy.mixin {
             pagenum: this.pagenum,
             pagesize: this.pagesize
         })
+        //下拉数据获取完成
+        callback && callback()
         // this.goodsList.push.apply(this.goodsList,data.message.goods);
         this.goodsList.push(...data.message.goods);
         this.total = data.message.total
@@ -46,4 +55,15 @@ export default class extends wepy.mixin {
         this.pagenum++
         this.getGoodsList()
     }
+
+    onPullDownRefresh() {
+        console.log('下拉了')
+        this.pagenum = 1
+        this.isLoading = false 
+        this.isMore = true
+        this.goodsList = []
+        this.getGoodsList(function(){
+            wepy.stopPullDownRefresh()
+        })
+      }
 }
