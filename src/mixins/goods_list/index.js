408c1ba36c70da3f56d5168404b5b79f074768ea
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-11-05 10:18:27 +0800

    商品列表-初始化
    商品列表-初始化

---
+++ src/mixins/goods_list/index.js
@@ -0,0 +1,15 @@
+import wepy from 'wepy'
+
+export default class extends wepy.mixin {
+    data = {
+    }
+
+    methods = {
+
+    }
+
+    onLoad(option){
+        console.log(option);
+        
+    }
+}
