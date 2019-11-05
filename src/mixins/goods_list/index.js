35a9bc126522f61711195eef862e4b0ddb1cc140
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-11-05 11:25:11 +0800

    商品列表-获取并渲染数据 下拉获取
    商品列表-获取并渲染数据 下拉获取

--- src/mixins/goods_list/index.js
+++ src/mixins/goods_list/index.js
@@ -2,14 +2,46 @@ import wepy from 'wepy'
 
 export default class extends wepy.mixin {
     data = {
+        query: '',
+        cid: '',
+        pagenum: 1,
+        pagesize: 10,
+        goodsList: [],
+        total: 0,
+        isLoading: false
     }
 
     methods = {
 
     }
 
-    onLoad(option){
-        console.log(option);
-        
+    /**
+     * 获取商品列表数据
+     */
+    async getGoodsList(){
+        this.isLoading = true;
+        const { data } = await wepy.get('/goods/search',{
+            query: this.query,
+            cid: this.cid,
+            pagenum: this.pagenum,
+            pagesize: this.pagesize
+        })
+        // this.goodsList.push.apply(this.goodsList,data.message.goods);
+        this.goodsList.push(...data.message.goods);
+        this.total = data.message.total
+        this.$apply()
+        this.isLoading = false
+    }
+
+    onLoad(options){
+        this.query = options.query || ''
+        this.cid = options.cat_id || ''
+        this.getGoodsList()
+    }
+
+    onReachBottom(){
+        if(this.isLoading) return
+        this.pagenum++
+        this.getGoodsList()
     }
 }
