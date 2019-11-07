5b27153702ec2ef776210d5f2ba5637995b9c754
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-11-07 15:37:28 +0800

    购物车-结构数据渲染
    购物车-结构数据渲染

--- src/mixins/tabs/cart.js
+++ src/mixins/tabs/cart.js
@@ -6,10 +6,25 @@ export default class extends wepy.mixin {
     }
 
     methods = {
-
+        /**
+         * 改变商品数量
+         */
+        countChange(event){
+            this.$parent.updateCount(event.target.dataset.id,event.detail);
+        },
+        
+        /**
+         * 改变是否购买商品状态
+         */
+        isCheckChange(event){
+            this.$parent.updateIsCheck(event.target.dataset.id,event.detail);
+        }
     }
 
     computed = {
+        /**
+         * 购物车是否为空
+         */
         isEmpty(){
             return this.cart.length == 0
         }
