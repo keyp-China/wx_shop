552f8310a6159fa04243d055760c2a8b38c8aaff
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-11-08 19:59:40 +0800

    提交订单-创建订单 发起预支付 判断支付状态 跳转
    提交订单-创建订单 发起预支付 判断支付状态 跳转

--- src/mixins/order/index.js
+++ src/mixins/order/index.js
@@ -26,20 +26,68 @@ export default class extends wepy.mixin {
          * 获取用户信息
          */
         async getUserInfo(even){
-            // const {encryptedData,iv,rawData,signature} = even.detail
-            // const {code} = await wepy.login();
-            // // 发送请求
-            // const res = await wepy.post("/users/wxlogin",{
-            //     encryptedData,iv,rawData,signature,code
-            // })
-            // if(res.data.meta.status !== 200) {
-            //     return wepy.baseToast('登录失败')
-            // }
-            // wepy.baseToast('登录成功')
-            // this.token = res.data.message.token
+            /* const {encryptedData,iv,rawData,signature} = even.detail
+            const {code} = await wepy.login();
+            // 发送请求
+            const res = await wepy.post("/users/wxlogin",{
+                encryptedData,iv,rawData,signature,code
+            })
+            if(res.data.meta.status !== 200) {
+                return wepy.baseToast('登录失败')
+            }
+            wepy.baseToast('登录成功')
+            this.token = res.data.message.token */
             this.token = 'TOKEN'
             wepy.setStorageSync('token',this.token)
             this.$apply()
+        },
+
+        /**
+         * 提交订单
+         */
+        async submitOrder(){
+            if(this.isShowAddress){
+                return wepy.baseToast("请选择收货地址")
+            }
+
+            // 准备数据 用于获取预支付订单号
+            const payOrderData = {
+                order_price: this.getTotalPirce,
+                consignee_addr: this.addressToString,
+                order_detail: JSON.stringify(this.cart),
+                goods: this.cart.map(item=>({
+                    goods_id:item.id,
+                    goods_number:item.count,
+                    goods_price: item.price/100
+                }))
+            }
+
+            // 1.创建订单
+            const { data } = await wepy.post('my/orders/create',payOrderData)
+            if(data.meta.status !== 200) {
+                return wepy.baseToast('创建订单失败')
+            }
+
+            // 2.创建订单成功后，发起生成预支付订单请求
+            const { data: prePayData } = await wepy.post('my/orders/req_unifiedorder', {
+                order_number: data.message.order_number
+            })
+
+            if (prePayData.meta.status !== 200) {
+                return wepy.baseToast('网络异常，重新支付')
+            }
+
+            const { errMsg } = await wepy.requestPayment(prePayData.message.pay).catch(err => err)
+            // 3.判断支付状态
+            if (errMsg === 'requestPayment:fail cancel') {
+                return wepy.baseToast('您已取消支付')
+            }
+
+            wepy.baseToast('支付成功')
+            // 支付成功后跳转到订单列表页
+            /* wepy.navigateTo({
+                url: '/pages/orderList/index'
+            }) */
         }
 
     }
