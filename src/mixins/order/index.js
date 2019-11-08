49c35a4eea5e76fee23c5f4e1e4f9bb11bf24af8
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-11-08 14:41:48 +0800

    提交订单-选择收货地址并渲染数据
    提交订单-选择收货地址并渲染数据

--- src/mixins/order/index.js
+++ src/mixins/order/index.js
@@ -1,10 +1,43 @@
 import wepy from 'wepy'
 
 export default class extends wepy.mixin {
-    data = {}
-
+    data = {
+        addressInfo: null
+    }
 
     methods = {
+        /**
+         * 选择收货地址
+         */
+        async chooseAddress(){
+            const res = await wepy.chooseAddress().catch(err=>err)
+            console.log(res);
+            if(res.errMsg !== 'chooseAddress:ok'){
+                return wepy.baseToast("您已取消选择收货地址")
+            }
+            this.addressInfo = res
+            wepy.setStorageSync("address",this.addressInfo)
+            this.$apply()
+        }
 
+    }
+
+    computed = {
+        isShowAddress(){
+            return this.addressInfo==null
+        },
+
+        /**
+         * 获取地址string
+         */
+        addressToString(){
+            if (this.addressInfo == null) return null
+            const { provinceName, cityName, countyName, detailInfo } = this.addressInfo
+            return provinceName + cityName + countyName + detailInfo
+        }
+    }
+
+    onLoad(){
+        this.addressInfo = wepy.getStorageSync("address") || null
     }
 }
