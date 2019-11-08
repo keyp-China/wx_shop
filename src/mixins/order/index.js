39e2a10e5e6f336fb92ceb6d3b71e93803d492e4
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-11-08 16:21:22 +0800

    提交订单-登录
    提交订单-登录

--- src/mixins/order/index.js
+++ src/mixins/order/index.js
@@ -2,7 +2,9 @@ import wepy from 'wepy'
 
 export default class extends wepy.mixin {
     data = {
-        addressInfo: null
+        addressInfo: null,
+        cart: [],
+        token: ''
     }
 
     methods = {
@@ -18,6 +20,26 @@ export default class extends wepy.mixin {
             this.addressInfo = res
             wepy.setStorageSync("address",this.addressInfo)
             this.$apply()
+        },
+
+        /**
+         * 获取用户信息
+         */
+        async getUserInfo(even){
+            // const {encryptedData,iv,rawData,signature} = even.detail
+            // const {code} = await wepy.login();
+            // // 发送请求
+            // const res = await wepy.post("/users/wxlogin",{
+            //     encryptedData,iv,rawData,signature,code
+            // })
+            // if(res.data.meta.status !== 200) {
+            //     return wepy.baseToast('登录失败')
+            // }
+            // wepy.baseToast('登录成功')
+            // this.token = res.data.message.token
+            this.token = 'TOKEN'
+            wepy.setStorageSync('token',this.token)
+            this.$apply()
         }
 
     }
@@ -25,6 +47,9 @@ export default class extends wepy.mixin {
     computed = {
         isShowAddress(){
             return this.addressInfo==null
+        },
+        isShowLogin(){
+            return this.token == ''
         },
 
         /**
@@ -34,10 +59,23 @@ export default class extends wepy.mixin {
             if (this.addressInfo == null) return null
             const { provinceName, cityName, countyName, detailInfo } = this.addressInfo
             return provinceName + cityName + countyName + detailInfo
+        },
+
+        /**
+         * 计算总价格
+         */
+        getTotalPirce(){
+            var total = 0
+            this.cart.forEach(item => {
+                total += item.price * item.count
+            });
+            return total*100
         }
     }
 
     onLoad(){
         this.addressInfo = wepy.getStorageSync("address") || null
+        this.cart = this.$parent.globalData.cart.filter(item=>item.isCheck)
+        this.token = wepy.getStorageSync("token") || ''
     }
 }
